b88921aa1d556775625778c776adc5c5aa7245ca
okboy <okboy_2008@msn.com>
2024-09-29 15:57:43 +0800

    add timer flag

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -20,7 +20,7 @@ const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "canvaskit/chromium/canvaskit.js": "671c6b4f8fcc199dcc551c7bb125f239",
 "canvaskit/chromium/canvaskit.js.symbols": "a012ed99ccba193cf96bb2643003f6fc",
 "canvaskit/canvaskit.js.symbols": "48c83a2ce573d9692e8d970e288d75f7",
-"main.dart.js": "68c65ad37ae5599154a9b1bdf98128ca",
+"main.dart.js": "610f46ef1aed06c406f61a244f6dce05",
 "assets/FontManifest.json": "7b2a36307916a9721811788013e65289",
 "assets/NOTICES": "cd93303a60c63c6bb7ca24d1288cb0b8",
 "assets/assets/3valley/runs.geojson": "733a3a24260e9f316658c506ec36713a",
@@ -33,7 +33,7 @@ const RESOURCES = {"favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "version.json": "7496e0dc6d345227097a68639fa42314",
 "manifest.json": "7011bc3accd45839d0a044607bfea229",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
-"flutter_bootstrap.js": "a28389c884e40a2f83f5bbf44e922350"};
+"flutter_bootstrap.js": "d94c1efbc2aae19e12c45510b007611b"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
