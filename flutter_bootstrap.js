c31d5f2076902492833988f0412da15ded23d63e
okboy <okboy_2008@msn.com>
2024-10-16 13:59:24 +0800

    using noto font

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1622262500"
+    serviceWorkerVersion: "3036079564"
   }
 });
