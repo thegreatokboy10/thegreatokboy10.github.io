b0f437eb433ee9190681abbcfe2bd3c8450a23e3
okboy <okboy_2008@msn.com>
2024-11-20 18:36:06 +0800

    do not trigger search if user is typing

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "2bff69f40d65c46eef45f36c3377298e",
+const RESOURCES = {"flutter_bootstrap.js": "88113b2265f69fe1606a28acb457dd2e",
 "version.json": "7496e0dc6d345227097a68639fa42314",
 "index.html": "d6d3dad9ec936979b5dcd2a1471d79eb",
 "/": "d6d3dad9ec936979b5dcd2a1471d79eb",
-"main.dart.js": "00751848fcdead90675581c1295100ff",
+"main.dart.js": "bd8ea1d0267f4f7c1af1cd309a3da09a",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
