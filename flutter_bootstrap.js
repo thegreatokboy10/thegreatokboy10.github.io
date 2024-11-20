3ea51a4d4e3f233c44f2bf8f218d1458732dc447
okboy <okboy_2008@msn.com>
2024-11-20 18:04:08 +0800

    add search

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3619334683"
+    serviceWorkerVersion: "1704270271"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "17f60770b061a81022b56cb7b6584852",
+const RESOURCES = {"flutter_bootstrap.js": "2bff69f40d65c46eef45f36c3377298e",
 "version.json": "7496e0dc6d345227097a68639fa42314",
 "index.html": "d6d3dad9ec936979b5dcd2a1471d79eb",
 "/": "d6d3dad9ec936979b5dcd2a1471d79eb",
-"main.dart.js": "e478ebc970d5994c9b3c7ccbfbd6de0e",
+"main.dart.js": "00751848fcdead90675581c1295100ff",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
