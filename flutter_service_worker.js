a75ca3671deb246effdc066bd8a915e87116771f
okboy <okboy_2008@msn.com>
2025-02-13 21:36:53 +0100

    update route instruction panel

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "f8bd2f5d44a2c1eb8c3f90bea91c119f",
+const RESOURCES = {"flutter_bootstrap.js": "f251854cede861bb11072ea7615987ee",
 "version.json": "7496e0dc6d345227097a68639fa42314",
 "index.html": "d6d3dad9ec936979b5dcd2a1471d79eb",
 "/": "d6d3dad9ec936979b5dcd2a1471d79eb",
-"main.dart.js": "d2dc74f30fecd97e28c5e1dedeb14d8e",
+"main.dart.js": "909539032a2e0ef6154839018311012a",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
