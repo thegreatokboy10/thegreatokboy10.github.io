d4a434756bb846a62d2d956d487027283c3fd661
okboy <okboy_2008@msn.com>
2025-02-26 10:00:32 +0100

    add loading screen

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "1e6a71308d7726e329bdb718ec4b3e85",
+const RESOURCES = {"flutter_bootstrap.js": "6806c243c46a4c4d6e55f5e8f12b4b4b",
 "version.json": "7496e0dc6d345227097a68639fa42314",
 "index.html": "d6d3dad9ec936979b5dcd2a1471d79eb",
 "/": "d6d3dad9ec936979b5dcd2a1471d79eb",
-"main.dart.js": "a74f701f5365bbc41c492d3a0b4fbbd9",
+"main.dart.js": "d68ce3b97a4fccd7026c191b2b9b4c5b",
 "flutter.js": "f393d3c16b631f36852323de8e583132",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
